8372e1278de068a449343cfba1cfc9c8d3302363
Shounak <shounakpurnapatre1@gmail.com>
2023-09-16 17:27:57 +0530

    Version 1.0 (Major update on frontdashboard)Completed the static image and hover of gif. Also added some animations

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,14 +1,14 @@
 import { Container, Typography } from "@mui/material";
 import React from "react";
 import "../styles/DashboardStyle.css";
-import 'animate.css';
+import "animate.css";
 
 function Dashboard() {
   return (
     <div className="dashboardmain">
-      <Container className="hello">
-        <Typography className="text">Hello🖐, I am Shounak!</Typography>
-        <Typography className="text2">
+      <Container className="hello    ">
+        <Typography className="text  animate__animated   animate__tada">Hello🖐, I am Shounak!</Typography>
+        <Typography className="text2 animate__animated animate__lightSpeedInLeft">
           Full-Stack Pixel 🎨 Painter 🌐
           <br />
           <br />
@@ -17,19 +17,9 @@ function Dashboard() {
           Design and Code 🎉✨
         </Typography>
       </Container>
-      <Container className="secondcontainer">
-        <div className="image-container">
-          <img
-            className="static-image"
-            alt="" 
-            src="/images/sampleimg.jpg"
-          />
-          <img
-            className="animated-gif"
-            alt="" 
-            src="/images/sample.gif"
-          />
-        </div>
+      <Container className="secondcontainer animate__animated animate__fadeIn">
+        <img className="static-image" alt="" src="/images/sampleimg.jpg" />
+        <img className="animated-gif" alt="" src="/images/sample.gif" />
       </Container>
     </div>
   );
